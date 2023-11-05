36624303f8ee45dfbf7bace56b350cce268cfbbd
Nixuge <mail@nixuge.me>
2023-11-05 18:07:09 +0100

    Info/InfoList design should be good enough

--- src/components/selector/infos/Info.vue
+++ src/components/selector/infos/Info.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-// defineProps<{
-//     html: string
-// }>()
+defineProps<{
+    title: string
+}>()
 </script>
 
 <template>
-    <slot />
+    <div class="info">
+        <h1>{{ title }}</h1>
+        <slot />
+    </div>
 </template>
+
+<style>
+.info {
+    padding: 10px;
+    margin-left: 1%;
+    margin-right: 1%;
+    background: rgba(0, 0, 0, 0.195);
+    width: 27%;
+    border-radius: 15px;
+    cursor: pointer;
+    margin-bottom: 20px;
+    height: 100px;
+}
+.info > * {
+    text-align: center;
+}
+</style>
--- src/components/selector/infos/InfoList.vue
+++ src/components/selector/infos/InfoList.vue
@@ -1,20 +1,98 @@
 <script setup lang="ts">
 import Info from './Info.vue'
 
+let currentOffset = 0;
+const offsetStep = 140;
+
+function moveValid(elem: HTMLElement, up: boolean) {
+    //40=margins, 140=calculating 1 step late so assuming we're on the step after
+    const elemMaxScroll = elem.scrollHeight - 40 - 140; 
+
+    if (currentOffset == 0 && up)
+        return false;
+    
+    if (elemMaxScroll <= currentOffset && !up)
+        return false;
+    
+    return true;
+}
+
+function scrollInfos(up: boolean) {
+    const elem = document.getElementById("infos") as HTMLElement;
+    
+    if (!moveValid(elem, up))
+        return;
+
+    const offset = up ? -offsetStep : offsetStep;
+    
+    currentOffset = currentOffset + offset;
+
+    elem?.scrollTo({
+        top: currentOffset,
+        behavior: "smooth"
+    })
+}
+
 </script>
 
 <template>
-    <div id="infos" class="fade">
-        <Info>aaa</Info>
+    <div id="infoswrap">
+        <div id="infos" class="fade">
+            <Info title="DMCA1">aaa</Info>
+            <Info title="DMCA2">aaa</Info>
+            <Info title="DMCA3">aaa</Info>
+            <Info title="DMCA4">aaa</Info>
+            <Info title="DMCA5">aaa</Info>
+            <Info title="DMCA6">aaa</Info>
+            <Info title="DMCA7">aaa</Info>
+            <Info title="DMCA8">aaa</Info>
+            <Info title="DMCA9">aaa</Info>
+            <Info title="DMCA10">aaa</Info>
+        </div>
+        <div id="infoscroller">
+            <div id="scrollUp" @click="scrollInfos(true)">^</div>
+            <div id="scrollDown" @click="scrollInfos(false)" style="align-self: right;">^</div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    #infos {
+    #infoswrap {
+        width: 80%;
         margin: auto;
-        padding: 20px;
-        border-radius: 15px;
-        width: 70%;
         background: rgba(0, 0, 0, 0.195);
+        display: flex;
+        border-radius: 15px;
+    }   
+    #infos {
+        width: 95%;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        overflow-x: auto;
+        max-height: 120px;
+        justify-content: center;
+        overflow: hidden;
+    }
+    #infoscroller {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        vertical-align: center;
+    }
+    #infoscroller > * {
+        background: rgba(0, 0, 0, 0.195);
+        padding-left: 5px;
+        padding-right: 5px;
+        border-radius: 3px;
+        font-size: 120%;
+        cursor: pointer;
+        user-select: none;
+    }
+    #scrollDown {
+        transform: rotate(180deg);
+        margin-top: 5px;
     }
 </style>
