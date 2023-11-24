5c2fe38be081aa6585444393eef13cc2229a3af6
Nixuge <mail@nixuge.me>
2023-11-25 00:44:18 +0100

    Skywars C only maps, Skywars & sg unparsed maps

--- src/ts/servers/mineplex/MineplexMeta.ts
+++ src/ts/servers/mineplex/MineplexMeta.ts
@@ -14,12 +14,12 @@ export class MineplexMeta implements IServerMeta {
 Comment with a dirty regex to add nekoli's parsed maps,
 just ignore this
 
-^    new M\(K\[(38)\], "(.*?)", "(.*?)", \[new D\(Z, "(.*?)"\)\], ([0-9]*?), false\)
+^    new M\(K\[(1)\], "(.*?)", "(.*?)", \[new D\(Z, "(.*?)"\)\], ([0-9]*?), false\)
 
     new M(K[$1], "$2", "$3", [new D(Z, "$4"), new D(P, "nekoli_parsed/$4", "nekoli")], $5, false)
 
 
-new D\(P, "nekoli_parsed\/(.*?).zip", "nekoli"\)
+, new D\(P, "nekoli_parsed\/([a-zA-Z0-9_]*?).zip", "nekoli"\)\]
 
-new D(P, "nekoli_parsed/$1.zip", "nekoli"), new D(U, "nekoli_unparsed/$1.zip", "nekoli")
+, new D(P, "nekoli_parsed/$1.zip", "nekoli"), new D(U, "nekoli_unparsed/$1.zip", "nekoli")]
 */
